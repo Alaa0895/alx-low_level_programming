925794337fa7d7714bc1b46c95d8b78954263c56
Alaa0895 <alaaems.95@gmail.com>
2023-12-15 14:39:10 -0800

    3-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - print alpha in caps and low case
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char Lower = 'a';
+	char Upper = 'A';
+
+	while (Lower <= 'z')
+	{
+		putchar(Lower);
+		Lower++;
+	}
+	while (Upper <= 'z')
+	{
+		putchar(Upper);
+		Upper++;
+	}
+	putchar('\n');
+	return (0);
+
