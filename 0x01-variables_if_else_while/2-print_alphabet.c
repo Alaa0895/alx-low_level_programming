1bb13a2175e9ad90189ab12cf260e3a31384f5b6
Alaa0895 <alaaems.95@gmail.com>
2023-12-15 16:54:42 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,19 @@
 
 /**
  * main - Entry point, prints the alphabet in lowercase
- * Return: Always 0 (Success) 
+ * Return: Always (0) 
  */
 int main(void)
 {
 	char i = 'a';
 
-	while (i <= 'z')
+	while(i <= 'z')
 	{
-	 putchar(i);
-	 i++;
+		putchar(i);
+		i++;
 	}
 
 	putchar('\n');
 
         return (0);
-
 }
