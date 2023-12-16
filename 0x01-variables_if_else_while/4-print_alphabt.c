9ae7b9f4af259a350e14af4a9af6d104b06691b3
Alaa0895 <alaaems.95@gmail.com>
2023-12-15 18:40:00 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
 	char low, e, q;	
@@ -13,13 +17,9 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != e && low != q)
-		{
-		     	putchar(low);
-		}
+	if (low != e && low != q)
+	putchar(low);
 	}
-
 	putchar('\n');
-
 	return (0);
 }
