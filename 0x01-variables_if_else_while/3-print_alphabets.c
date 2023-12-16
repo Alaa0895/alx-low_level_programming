81e1bc676685e66c3ce44bb408cdc501ca62e349
Alaa0895 <alaaems.95@gmail.com>
2023-12-15 18:48:10 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
 
 /**
  * main - Entry print
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
+	putchar(i);
 
 	for (i = 'A'; i <= 'Z'; i++)
-		putchar(i);
+	putchar(i);
 	putchar('\n');
+
 	return (0);
 }
