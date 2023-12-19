5fa9b9b17d720c4a128354f33bd3470e046d8d31
Alaa0895 <alaaems.95@gmail.com>
2023-12-19 15:59:30 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,21 +6,9 @@
  * @n2: The second number to be computed
  * Return: Integer value
  */
-int add(int n1, int n2);
 
-int main(void)
+int add (int n1 , int n2);
 {
-	int result;
-	int n1 = 89;
-	int n2 = 9;
-
-	result = add(n1, n2);
-
-	printf("Result: %d\n", result);
-
-	return (0);
-}
-int add(int n1 + int n2);
-{
+	n = add(89, 9);
 	return (n1 + n2);
 }
