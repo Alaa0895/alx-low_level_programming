589ea6da48bafb38863b85bc11683c6eceb9196c
Alaa0895 <alaaems.95@gmail.com>
2023-12-19 15:53:18 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer value
+ */
+int add(int n1, int n2);
+
+int main(void)
+{
+	int result;
+	int n1 = 89;
+	int n2 = 9;
+
+	result = add(n1, n2);
+
+	printf("Result: %d\n", result);
+
+	return (0);
+}
+int add(int n1 + int n2);
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -6,6 +6,7 @@
  * Return: Always 0.
  */
 int main(void)
+
 {
 	print_to_98(0);
 	print_to_98(98);
