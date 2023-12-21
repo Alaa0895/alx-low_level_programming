60f289466c99aa6c885b521f1049083b6e9daefb
Alaa0895 <alaaems.95@gmail.com>
2023-12-20 17:09:31 -0800

    0-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ *  Return: 0
+ */
+
+int main (void)
+{
+	int i;
+	printf("Infinite loop incoming :(\n");
+	
+	1 = 0;
+	while(i < 10)
+	{
+		putchar (i);
+	}
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,12 @@
+#include "stdio.h"
 #include "main.h"
 
-/**
- * main - Determine if anumber is positive, negative or zero.
- * 0: is the number to be checked
- * Return: '0' on (Success)
- */
 void positive_or_negative(int i)
 {
-	if (i < 0)
-{
-	printf("%d is %s\n", i, "negative");
-}
-	else if (i > 0)
-{
-	printf("%d is %s\n", i, "positive");
-}
-	else (i = 0)
-{
-	printf("%d is %s\n", i, "zero");
-}
-return;
-
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i)
 }
