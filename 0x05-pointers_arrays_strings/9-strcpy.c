92f208963955863bf75e2eb701b447e058a55e75
Alaa0895 <alaaems.95@gmail.com>
2024-01-02 14:56:32 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,14 +12,14 @@ char *_strcpy(char *dest, char *src)
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
 	for ( ; x < l ; x++)
 	{
-	dest[x] = src[x];
+		dest[x] = src[x];
 	}
-	dest[1] = '\0';
+	dest[l] = '\0';
 	return (dest);
 }
