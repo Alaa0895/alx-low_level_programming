a0b1d0c01fbfa4ee4febd31ba0253907942bc89c
Alaa0895 <alaaems.95@gmail.com>
2024-01-02 14:51:16 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * char *_strepy - a function that copies the string pointed to by src
+ * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
- */ 
+ */
 
-char * _stropy(char *dest, char*src)
+char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
