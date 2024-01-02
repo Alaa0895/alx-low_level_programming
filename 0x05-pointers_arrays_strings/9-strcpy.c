55a2057e62842b64679d3f7232e6dd99337ada8c
Alaa0895 <alaaems.95@gmail.com>
2024-01-02 14:24:36 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer.
+ * _atoi - Convert a string to an integer
  * @s: The pointer to convert
  * Return: A integer
  */
 
 int _atoi(char *s)
+
 {
 
 	int c = 0;
