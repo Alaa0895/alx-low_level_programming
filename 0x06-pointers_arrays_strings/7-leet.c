1e6673745e69656ee9418cca1a9144d1b3d751d4
Alaa0895 <alaaems.95@gmail.com>
2024-01-04 12:58:21 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,10 +3,10 @@
 /**
  * leet - function that encodes a string into 1337.
  * letters a and A are replaced by 4.
- * Letters e and E are replaced by 3.
- * Letters o and O are replaced by 0.
- * Letters t and T are replaced by 7.
- * Letters l and L are replaced by 1.
+ * Letters e and E are replaced by 3
+ * Letters o and O are replaced by 0
+ * Letters t and T are replaced by 7
+ * Letters l and L are replaced by 1
  * @s: pointer to string.
  *
  * Return: pointer to s.
@@ -20,7 +20,6 @@ char *leet(char *s)
 
 /* scan through string */
 	string length = 0;
-
 	while (s[string_length] != '\0')
 /* check whether leetLetter is found */
 	{
