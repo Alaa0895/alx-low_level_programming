06454d45add6eb9a17605010275837469b6bb175
Alaa0895 <alaaems.95@gmail.com>
2024-01-04 12:39:26 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * leet - function that encodes a string into 1337.
- * letters a and A are replaced by 4.
- * Letters e and E are replaced by 3
- * Letters o and O are replaced by 0
- * Letters t and T are replaced by 7
- * Letters l and L are replaced by 1
+ * Letters a and A are replaced by 4.
+ * Letters e and E are replaced by 3.
+ * Letters o and O are replaced by 0.
+ * Letters t and T are replaced by 7.
+ * Letters l and L are replaced by 1.
  * @s: pointer to string.
  *
  * Return: pointer to s.
