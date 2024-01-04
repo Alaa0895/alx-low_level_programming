aed87a6e65a604f531fb5f66c79259ae063368a4
Alaa0895 <alaaems.95@gmail.com>
2024-01-04 12:47:44 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 
 /**
  * leet - function that encodes a string into 1337.
- * Letters a and A are replaced by 4.
+ * letters a and A are replaced by 4.
  * Letters e and E are replaced by 3.
  * Letters o and O are replaced by 0.
  * Letters t and T are replaced by 7.
