a596d7883d21a9bf5b6a52d0c2735594c7691681
Alaa0895 <alaaems.95@gmail.com>
2024-01-04 09:13:32 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,10 +2,15 @@
 
 /**
  * _strncat - Concatenates two strings using at most
+ *
  * an inputted number of bytes from src.
+ *
  * @dest: The string to be appended upon.
+ *
  * @src: The string to be appended to dest.
- * @n: The number of bytes from sre to be appended to dest.
+ *
+ * @n: The number of bytes from src to be appended to dest.
+ *
  * Return: A pointer to the resulting string dest.
  */
 
@@ -16,7 +21,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[index++])
 	dest_len++;
-	for (index = src[index] && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 	dest[dest_len++] = src[index];
 	return (dest);
 }
