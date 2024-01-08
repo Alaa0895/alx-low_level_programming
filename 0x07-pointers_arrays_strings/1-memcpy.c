f1412320fb63f6d32bbfd559748b8cbe1960ab82
Alaa0895 <alaaems.95@gmail.com>
2024-01-08 07:52:09 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,4 @@
-#1nclude "main.h"
+#include "main.h"
 /**
  * _memcpy - Entry point
  * @dest: input
@@ -11,6 +11,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	dest[i] = srclil;
+	dest[i] = src[i];
 	return (dest);
 }
