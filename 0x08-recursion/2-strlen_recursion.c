a2e52547a340c0ef5fc71795f06f21811fa6087a
Alaa0895 <alaaems.95@gmail.com>
2024-01-10 11:20:29 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strlen_recursion - Calculate the length of a string 
+ * _strlen_recursion - Calculate the length of a string
  * @s: the string to count
  *
  * Return: integer value
  */
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
