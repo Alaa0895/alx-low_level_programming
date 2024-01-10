01ae13581fd67e561a133b95c7c25b3fc68854e3
Alaa0895 <alaaems.95@gmail.com>
2024-01-10 10:43:57 -0800

    0-puts_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * _print_rev_recursion - Print a string in reverse
- * @*s: the string to reverse
+ * @s: the string to reverse
  * Return: Nothing.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s = '\0')
+	if (s = '\0')
 	{
 	return;
 	}
