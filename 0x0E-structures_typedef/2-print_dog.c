db222b1da0bec8bba0d21ca403aaf466a5d1256f
Alaa0895 <alaaems.95@gmail.com>
2024-01-22 12:56:34 -0800

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/** 
+ * print_dog - prints a dog
+ * @d: the dog to print
+ * Return: void.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %5\n", d->owner ? d-›owner : "(nil)");
+	}
+}
