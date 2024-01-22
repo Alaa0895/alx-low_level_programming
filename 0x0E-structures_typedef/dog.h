ac58a16d375be009d49f0ece258e609e9db39b57
Alaa0895 <alaaems.95@gmail.com>
2024-01-22 12:25:48 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * downer do'seas name
+ * Description: just a lone dog struct in a big kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
