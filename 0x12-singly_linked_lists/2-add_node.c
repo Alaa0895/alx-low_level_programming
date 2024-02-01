7cd443d23551fb7dd7b48917979ee656752b88a0
Alaa0895 <alaaems.95@gmail.com>
2024-02-01 09:38:27 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (str)
 	{
 		new_head->str = strdup(str);
-		if (new_head->str)
+		if (!new_head->str)
 		{
 			free(new_head);
 			return (NULL);
